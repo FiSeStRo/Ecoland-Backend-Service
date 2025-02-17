524c4d91dee844f399c8830f86b09b59b29f94aa
Finny_ST <58487697+FinnyChSt@users.noreply.github.com>
2025-02-17 22:41:18 +0200

    add read jwt from env

--- go/authentication/authentication.go
+++ go/authentication/authentication.go
@@ -14,25 +14,32 @@ type EcoUserClaims struct {
 	jwt.RegisteredClaims
 }
 
+var JwtVariables struct {
+	Key              []byte
+	ExpirationTime   int
+	ExpirationTimeRT int
+	Issuer           string
+}
+
 // TODO: implement rotating keys with a db
-var jWTKey = []byte("testKeyEcoLand")
 
 // CreateNewJWT creates a new jwt with a userId payload and set's the expiration times based on if the creation should be a refreshToken
 func CreateNewJWT(userId int, isRT bool) (string, error) {
-	expirationTime := 5 * time.Minute
+	expirationTime := time.Duration(JwtVariables.ExpirationTime) * time.Second
 	if isRT {
-		expirationTime = 12 * time.Minute
+		expirationTime = time.Duration(JwtVariables.ExpirationTimeRT) * time.Second
 	}
 	claims := EcoUserClaims{
 		userId,
 		jwt.RegisteredClaims{
+			Issuer:    JwtVariables.Issuer,
 			IssuedAt:  jwt.NewNumericDate(time.Now()),
 			ExpiresAt: jwt.NewNumericDate(time.Now().Add(expirationTime)),
 		},
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS512, claims)
-	st, err := token.SignedString(jWTKey)
+	st, err := token.SignedString(JwtVariables.Key)
 	if err != nil {
 		log.Println("jwt error", err)
 		return "", fmt.Errorf("could not create and sign jwt %w", err)
@@ -44,7 +51,7 @@ func CreateNewJWT(userId int, isRT bool) (string, error) {
 // ValidateJWT validates a given jwt and return the EcoUserClaims if valid
 func ValidateJWT(t string) (EcoUserClaims, error) {
 	vt, err := jwt.ParseWithClaims(t, &EcoUserClaims{}, func(t *jwt.Token) (interface{}, error) {
-		return jWTKey, nil
+		return JwtVariables.Key, nil
 	})
 
 	isValid := err == nil && vt.Valid
