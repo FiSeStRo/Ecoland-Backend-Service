8444662f9e6ce6e4d7146cda7d8fa86c983f0f22
nii42 <nii42@gmx.de>
2025-03-01 20:14:34 +0100

    first part of actual requesting building details command

--- php/endpoints/buildings_endpoint.php
+++ php/endpoints/buildings_endpoint.php
@@ -7,6 +7,7 @@ class BuildingEndpoint extends Endpoint {
         parent::__construct($command, $params, $authHandler);
 
         $this->registerCommand('construct', 'constructBuilding', CommandType::PostJson, UserLevel::User);
+        $this->registerCommand( 'details', 'getBuildingDetails', CommandType::GetWithId, UserLevel::User);
     }   
 
     public function constructBuilding() : InternalStatus{
@@ -21,6 +22,54 @@ class BuildingEndpoint extends Endpoint {
         $buildingService = new BuildingService;
         $status = $buildingService->constructBuilding($defId, $userId, $displayName);
         return $status;
+    }
+
+    public function getBuildingDetails() : InternalStatus{
+        $buildingId = $this->getParams()['id'];
+        $userId = $this->getCurrentUserId();
+
+        $buildingService = new BuildingService;
+        $validatationStatus = $buildingService->doesBuildingBelongToUserId($buildingId, $userId);
+        if( !$validatationStatus->isValidStatus()){
+            return $validatationStatus;
+        }
+       
+        // Request all the required data from the different service and build the data for the response.
+        $buildingStatus = $buildingService->getBuildingDetails($buildingId);
+        if(!$buildingStatus->isValidStatus()){
+            return $buildingStatus;
+        }   
+        $buildingData = $buildingStatus->getData();
+        $buildingDefinition = null;        
+        $buildingDefinitionId = $buildingData['def_id'];
+        if( $buildingDefinitionId > 0){
+            $buildingDefinition = $buildingService->getDefinition(DefinitionType::Building, $buildingDefinitionId);
+        }
+
+        $productionService = new ProductionService;
+        $productionStatus = $productionService->getAllProductionsForBuildingId($buildingId);
+        if(!$productionStatus->isValidStatus()){
+            return $productionStatus;
+        }
+
+        $storageService = new StorageService;
+        $storageStatus = $storageService->getStorageForBuilding($buildingId);
+        if(!$storageStatus->isValidStatus()){
+            return $storageStatus;
+        }
+
+        $detailStatus = new InternalStatus(RequestStatus::ValidDatabaseRequest);
+        $detailData = (object)[];
+        $detailData->id = $buildingData['id'];
+        $detailData->name = $buildingData['name'];
+        $detailData->type = (object)[];
+        $detailData->type->def_id = $buildingData['def_id'];
+        $detailData->type->token_name = $buildingDefinition->token_name ?? '';
+
+        // #TODO: Implementation weiter
+
+        //dev_var_dump($detailData);
+        return new InternalStatus(RequestStatus::Undefined);
     }
 }
 
